2b2c825fe03c0171288a4cb17e95fa0b46191494
Saurabh Datta <s.datta@edu.ciid.dk>
2014-10-30 19:41:57 +0200

    basic data updated

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= '0IHu5rJLcLjQjyBgmoDDvydtQIQ1NTYjimxshwnFO9XppyOQ', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ['359779930'], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Dexter_Demo', // Replaces Xively logo in the header
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor		= '123740', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
